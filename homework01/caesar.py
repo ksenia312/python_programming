08ea8bc7446e82eb2680db78025833e34734e875
Ksenia <nikitina3619@gmail.com>
2020-10-30 11:04:09 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -4,7 +4,6 @@ import typing as tp
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -15,14 +14,26 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    shift=shift%26
+    ciphertext=list(plaintext)
+    for i in range(len(ciphertext)):
+        
+        ciphertext[i]=ord(ciphertext[i])
+        if 65 <= ciphertext[i] <= (90-shift) or 97 <= ciphertext[i] <= (122-shift):
+            ciphertext[i]=ciphertext[i]+shift
+            
+        elif (90-shift+1) <= ciphertext[i] <= 90 or (122-shift+1) <= ciphertext[i] <= 122:
+            ciphertext[i]=ciphertext[i]-(26-shift)
+            
+        ciphertext[i]=chr(ciphertext[i])
+    ciphertext=''.join(ciphertext)
+        
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
-
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -33,7 +44,20 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    shift=shift%26
+    plaintext=list(ciphertext)
+    for i in range(len(plaintext)):
+        
+        plaintext[i]=ord(plaintext[i])
+        
+        if (65+shift) <= plaintext[i] <= 90 or (97+shift) <= plaintext[i] <= 122:
+            plaintext[i]=plaintext[i]-shift
+            
+        elif 65 <= plaintext[i] <= (65+shift-1) or 97 <= plaintext[i] <= (97+shift-1):
+            plaintext[i]=plaintext[i]+26-shift
+            
+        plaintext[i]=chr(plaintext[i])
+    plaintext=''.join(plaintext)
     return plaintext
 
 
