f5c456c9364449950e1c3366cd45b87b792faf62
ksenia312 <ksynikitina.02@mail.ru>
2021-01-31 15:58:32 +0300

    all tests passed

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -8,16 +8,56 @@ from ui import UI
 class GUI(UI):
 
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
+        self.cell_size = cell_size
+        self.speed = speed
         super().__init__(life)
 
     def draw_lines(self) -> None:
-        # Copy from previous assignment
-        pass
+        for x in range(0, self.cols, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('#889ac2'), (x, 0), (x, self.rows))
+        for y in range(0, self.rows, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('#889ac2'), (0, y), (self.cols, y))
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
-        pass
+        grid = self.curr_generation
+        for i in range(self.self.rows):
+            for j in range(self.self.cols):
+                if grid[i][j] == 1:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color('#260d94'),
+                        pygame.Rect(j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                    )
+                else:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color('white'),
+                        pygame.Rect(j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                    )
 
     def run(self) -> None:
-        # Copy from previous assignment
-        pass
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        # self.screen.fill(pygame.Color('white'))
+        # Создание списка клеток
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            # Отрисовка списка клеток
+
+            self.draw_grid()
+            self.draw_lines()
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            self.curr_generation = self.get_next_generation()
+
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    game = GUI((5, 5))
+    game.run()
--- homework03/life.py
+++ homework03/life.py
@@ -3,19 +3,17 @@ import random
 
 from typing import List, Optional, Tuple
 
-
 Cell = Tuple[int, int]
 Cells = List[int]
 Grid = List[Cells]
 
 
 class GameOfLife:
-    
     def __init__(
-        self,
-        size: Tuple[int, int],
-        randomize: bool = True,
-        max_generations: Optional[float] = float('inf')
+            self,
+            size: Tuple[int, int],
+            randomize: bool = True,
+            max_generations: Optional[float] = float('inf')
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -28,37 +26,79 @@ class GameOfLife:
         # Текущее число поколений
         self.generations = 1
 
-    def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+    def create_grid(self, randomize: bool = True) -> Grid:
+        if not randomize:
+            grid1 = [[0 for i in range(self.cols)] for j in range(self.rows)]
+        else:
+            grid1 = [[random.randint(0, 1) for i in range(self.cols)] for j in range(self.rows)]
+        return grid1
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        cells_cort = []
+        cells = []
+        h = cell[0]
+        w = cell[1]
+        if w - 1 >= 0:
+            cells_cort.append((h, w - 1))
+        if w + 1 < self.cols:
+            cells_cort.append((h, w + 1))
+        if h - 1 >= 0:
+            cells_cort.append((h - 1, w))
+        if h + 1 < self.rows:
+            cells_cort.append((h + 1, w))
+        if h - 1 >= 0 and w - 1 >= 0:
+            cells_cort.append((h - 1, w - 1))
+        if h - 1 >= 0 and w + 1 < self.cols:
+            cells_cort.append((h - 1, w + 1))
+        if h + 1 < self.rows and w - 1 >= 0:
+            cells_cort.append((h + 1, w - 1))
+        if h + 1 < self.rows and w + 1 < self.cols:
+            cells_cort.append((h + 1, w + 1))
+        for k in cells_cort:
+            cells.append(self.curr_generation[k[0]][k[1]])
+        return cells
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        new_grid = []
+        for i in range(self.rows):
+            new_grid.append([])
+            for j in range(self.cols):
+                neighbours = self.get_neighbours((i, j))
+                if sum(neighbours) == 3:
+                    new_grid[i].append(1)
+                elif self.curr_generation[i][j] == 1 and sum(neighbours) == 2:
+                    new_grid[i].append(1)
+                else:
+                    new_grid[i].append(0)
+        self.generations += 1
+        return new_grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.prev_generation = self.curr_generation
+        self.curr_generation = self.get_next_generation()
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        k = True
+        if self.generations > self.max_generations:
+            k = False
+        return k
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        k = True
+        if self.prev_generation == self.curr_generation:
+            k = False
+        return k
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> 'GameOfLife':
@@ -72,3 +112,12 @@ class GameOfLife:
         Сохранить текущее состояние клеток в указанный файл.
         """
         pass
+
+'''
+a = 1
+while a != 0:
+    game = GameOfLife((5, 5))
+    grid = game.get_next_generation()
+    a = game.get_neighbours((0, 0))
+    print(grid)
+'''
